958cd9870594bbba2f9e4543397c7ed4856aa161
dina <dinaelhadidy966@gmail.com>
2023-10-09 17:46:42 -0400

    Handle 404 response

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Starts a new flask application"""
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -15,6 +15,12 @@ def teardown(_):
     """After each request remove the current SQLAlchemy Session
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def error(error):
+    """Handle not found error"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
