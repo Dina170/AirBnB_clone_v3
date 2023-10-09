8c1cf6e7e96a910f16d204daf50d89f7ad6bcee9
GalalMohammed <galalmoh2001@gmail.com>
2023-10-10 00:31:59 +0300

    create stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,28 @@
 """index view"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status")
 def status():
     """Get the status of api"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """retrieves the number of each objects by type"""
+    classes = {"amenities": Amenity, "cities": City,
+               "places": Place, "reviews": Review,
+               "states": State, "users": User}
+    objs = {}
+    for name, cls in classes.items():
+        objs[name] = storage.count(cls)
+    return jsonify(objs)
